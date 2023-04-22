535cfed9c5c162fc210ee99b692aa68ff12de921
Satyam <84779436+satyamjhadps1@users.noreply.github.com>
2023-04-22 15:48:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("My First JavaScript Program.");
+console.log("Mathematical Operations:  Numbers Are 100 and 10")
+var a = 100
+var b = 10;
+console.log("Addition: " + String(a + b))
+console.log("Subtraction: " + String(a - b))
+console.log("Multiplication: " + String(a * b))
+console.log("Division: " + String(a / b))
+console.log("Modulus: " + String(a % b))
